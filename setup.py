3f90ea0ca756cc4c2a162073ad505ff6a18faf8e
sayaksingha <sayaksingha123@gmail.com>
2023-09-11 16:29:17 +0530

    new add

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+setup(name="SAYAK_1ST_PROJECT",
+      version="0.0.1",
+      author="SAYAK SINGHA",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]
+      )
